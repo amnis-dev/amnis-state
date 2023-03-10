38211768a293db8013098d3c6e594294a91dff81
Eric Crowell <eric.crowell@ecrow.dev>
2023-03-10 13:38:55 +0100

    dev: updated configuration files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,3 @@
 module.exports = {
   extends: ['@amnis/eslint-config-node'],
-  rules: {
-    'import/no-cycle': [
-      'error',
-      {
-        maxDepth: 10,
-        ignoreExternal: true,
-      },
-    ],
-  },
 };
