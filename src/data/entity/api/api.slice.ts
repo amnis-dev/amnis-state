71a80d9ec5a19bd5625529036bc44388f4b2fb10
Eric Crowell <eric.crowell@ecrow.dev>
2023-03-16 17:41:32 +0100

    feat(Api): Added additional selectors for convenience

--- src/data/entity/api/api.slice.ts
+++ src/data/entity/api/api.slice.ts
@@ -1,3 +1,4 @@
+import type { EntityState } from '@reduxjs/toolkit';
 import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
 import { entityExtraReducers, entityReducers } from '../entity.reducers.js';
 import { entitySelectors } from '../entity.selectors.js';
@@ -6,6 +7,8 @@ import { metaInitial } from '../entity.js';
 import type { Entity } from '../entity.types.js';
 import { apiKey } from './api.js';
 import type { Api, ApiMeta } from './api.types.js';
+import type { State } from '../../../state.types.js';
+import type { System } from '../system/system.types.js';
 
 /**
  * RTK api adapter.
@@ -83,6 +86,27 @@ export const apiSelectors = {
    * Gets core selectors.
    */
   ...entitySelectors<Api>(apiKey),
+  /**
+   * Selects multiple apis from a system.
+   */
+  selectSystemApis: (state: State, system: System): Entity<Api>[] => {
+    const slice = state[apiKey] as ApiMeta & EntityState<Entity<Api>>;
+    const apis = Object.values(slice.entities).filter(
+      (entity) => !!entity && system.$apis.includes(entity.$id),
+    ) as Entity<Api>[];
+    return apis;
+  },
+  /**
+   * Selects a combo id by role ids.
+   */
+  selectSystemApi: (state: State, system: System, reducerPath: string): Entity<Api> | undefined => {
+    const slice = state[apiKey] as ApiMeta & EntityState<Entity<Api>>;
+    const apis = Object.values(slice.entities).filter(
+      (entity) => !!entity && system.$apis.includes(entity.$id),
+    ) as Entity<Api>[];
+    const api = apis.find((entity) => entity.reducerPath === reducerPath);
+    return api;
+  },
 };
 
 /**
