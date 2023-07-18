c68aa6a6327eb7ce160db5fcdc7023a33bcbdd9c
Eric Crowell <eric.crowell@ecrow.dev>
2023-07-18 21:28:48 +0200

    feat: Emailer now accepts templates

--- src/io/emailer/emailer.templates.ts
+++ src/io/emailer/emailer.templates.ts
@@ -1,26 +1,16 @@
-const emailerTemplates = {
-/**
- * Template for sending a one-time passcode.
- *
- * Examples
- * ```
- * onetimePasscode('123456', '10 minutes');
- * ```
- */
-  onetimePasscode: (code: string, lifetime: string) => `\
+import type { EmailerTemplates } from './emailer.types.js';
+
+const emailerTemplateOtp: EmailerTemplates['otp'] = ({ code, lifetime }) => `\
 Your one-time passcode is ${code}.
- 
+
 This code will expire in ${lifetime}.\
-`,
+`;
+
+export const emailerTemplates: EmailerTemplates = {
+  otp: emailerTemplateOtp,
 };
 
-/**
- * Gets send templates.
- */
-export const emailerGetTemplate = (key: keyof typeof emailerTemplates) => emailerTemplates[key];
-
-export default emailerGetTemplate;
-
+export default emailerTemplates;
 /**
  * Sets a new template or overrites one.
  */
